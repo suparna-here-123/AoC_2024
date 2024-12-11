5dd0023f34c47448b81f6f8e574f70766988710f
First Curiosity Rover <83354723+suparna-here-123@users.noreply.github.com>
2024-12-11 14:03:55 +0530

    Day 7 done

--- 2024/Day_7.py
+++ 2024/Day_7.py
@@ -17,7 +17,8 @@ def opCombos(s, maxLen) :
 
     withPlus = opCombos(s + '+', maxLen)
     withMul = opCombos(s + '*', maxLen)
-    return withPlus + withMul
+    withConcat = opCombos(s + '|', maxLen)
+    return withPlus + withMul + withConcat
 
 def partOne(allCalib) :
     res = []
@@ -30,11 +31,11 @@ def partOne(allCalib) :
             argsCopy = copy(args)
             cSum = int(argsCopy[0])
 
+            # MAKE THIS SIMPLER
             for i, char in zip(range(1, len(args) + len(combo), 2), combo) :
                 argsCopy.insert(i, char)
                 rightArg = int(argsCopy[i + 1])
                 cSum = cSum + rightArg if char == '+' else cSum * rightArg
-
 
             #exp = ''.join(argsCopy)
             #if eval(exp) == int(calib) :
@@ -43,12 +44,42 @@ def partOne(allCalib) :
                 break
     return sum(res)
 
+def partTwo(allCalib) :
+    res = []
+    for X in allCalib :
+        print(f"for {X}")
+        calib, args = X
+        allCombos = opCombos('', len(args) - 1)
+        
+        for combo in allCombos :
+            #print(f"Checking combo : {combo}")
+            argsCopy = copy(args)
+            cSum = int(argsCopy[0])
+
+            # MAKE THIS SIMPLER
+            for i, char in zip(range(1, len(args) + len(combo), 2), combo) :
+                argsCopy.insert(i, char)
+                rightArg = int(argsCopy[i + 1])
+
+                if char == '+' :
+                    cSum += rightArg
+                elif char == '*' :
+                    cSum *= rightArg
+                else :
+                    cSum = int(str(cSum) + str(rightArg))
+
+            if cSum == int(calib) :
+                res.append(int(calib))
+                break
+
+    return sum(res)
 
 def main() :
     filePath = Path(sys.argv[1])
     allCalib = parse(filePath)
     #allCalib = parse()
-    print(partOne(allCalib))
+    #print(partOne(allCalib))
+    print(partTwo(allCalib))
 
 if __name__ == '__main__' :
     main()
