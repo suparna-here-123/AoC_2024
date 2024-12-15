f5ce3c29804f8c1d8aca91a8af3eca7b5a009577
First Curiosity Rover <83354723+suparna-here-123@users.noreply.github.com>
2024-12-15 11:20:08 +0530

    reordered code

--- 2024/Day_10.py
+++ 2024/Day_10.py
@@ -1,20 +1,17 @@
-def partOne(row, col, num, seen) :
+def find(row, col, num, seen) :
     global m, n, grid
     if 0 <= row < m and 0 <= col < n :
         if grid[row][col] == str(num) :
-            #grid[row][col] = 'X'
-            print(f"{num} at {row}, {col}")
             if num == 9 :
-                #seen.add((row, col))
-                seen.append((row, col))
-                print(f"{num} at {row}, {col}")
+                #seen.add((row, col))       -> for part 1
+                seen.append((row, col))     # -> for part 2
                 print()
             
             else :
-                partOne(row-1, col, num+1, seen)
-                partOne(row+1, col, num+1, seen)
-                partOne(row, col-1, num+1, seen)
-                partOne(row, col+1, num+1, seen)
+                find(row-1, col, num+1, seen)
+                find(row+1, col, num+1, seen)
+                find(row, col-1, num+1, seen)
+                find(row, col+1, num+1, seen)
 
 def parseFile() :
     f = open(r'C:\Users\HP\OneDrive\Desktop\AdventOfCode\2024\Inputs\Day_10_input.txt', 'r')
@@ -34,9 +31,10 @@ for lineInd, line in enumerate(grid) :
     for charInd, char in enumerate(line) :
         if char == '0' :
             count = 0
-            # seen = set()
+            # seen = set()  
+            # find(lineInd, charInd, 0, set) -> part 1
             lst = []
-            partOne(lineInd, charInd, 0, lst)
+            find(lineInd, charInd, 0, lst)
             print(f"From trailhead at {lineInd}, {charInd} -> {lst}")
             final += len(lst)
 
