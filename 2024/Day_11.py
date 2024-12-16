2c3336f93c31534108ea39cae343367e1704fcc2
First Curiosity Rover <83354723+suparna-here-123@users.noreply.github.com>
2024-12-16 07:39:52 +0530

    Day 11 alternate method

--- 2024/Day_11.py
+++ 2024/Day_11.py
@@ -27,41 +27,66 @@ def applyRule(s : str) :
 
     return splitMap[s]
 
-ip = '2 72 8949 0 981038 86311 246 7636740'
-#ip = '125 17'
-stones = [int(x) for x in ip.split()]
+ip = '2 72 8949 0 981038 86311 246 7636740'.split()
+#ip = '125 17'.split()
+inBlink = {k : ip.count(k) for k in set(ip)}
+# print(inBlink)
 
-@cache                                                                  # 40% faster if you cache this also lol
-def anotherRule(stone : int) :
-    if stone == 0 :
-        return 1
-    
-    # If stone is even-length string
-    s = str(stone)
-    if len(s) % 2 == 0 :
-        return (int(s[:len(s)//2]), int(s[len(s)//2 :]))
-    
-    # If neither of above rules
-    return stone * 2024
-    
-@cache
-def count(stone, stepsRem) :
-    if stepsRem == 0 :              # No blinks remaining, just return the stone
-        return 1
-    
-    if stone == 0 :
-        return count(anotherRule(stone), stepsRem - 1)
+def withDict() :
+    global inBlink
+    for blink in range(75) :
+        #print(f"blink {blink}")
+        nextBlink = defaultdict(int)
+        for stone, stoneCount in inBlink.items() :
+            if int(stone) == 0 :
+                nextBlink['1'] += stoneCount
+            
+            elif len(stone) % 2 == 0 :
+                nextBlink[str(int(stone[ : len(stone)//2]))] += stoneCount
+                nextBlink[str(int(stone[len(stone)//2 : ]))] += stoneCount
+            
+            else :
+                nextBlink[str(int(stone) * 2024)] += stoneCount
+        
+        inBlink = nextBlink
+    print(sum(inBlink.values()))
 
-    # If stone is even-length string
-    s = str(stone)
-    if len(s) % 2 == 0 :
-        l, r = anotherRule(stone)
-        return count(l, stepsRem - 1) + count(r, stepsRem - 1)
+#print(inBlink)        
+execution_time = timeit.timeit(lambda: withDict)
+print(execution_time)
+
+# ----------------------- WITH CACHING -------------------------------
+# stones = [int(x) for x in ip.split()]
+# @cache                                                                  # 40% faster if you cache this also lol
+# def anotherRule(stone : int) :
+#     if stone == 0 :
+#         return 1
     
-    # If neither of above rules
-    return count(anotherRule(stone), stepsRem - 1)
+#     # If stone is even-length string
+#     s = str(stone)
+#     if len(s) % 2 == 0 :
+#         return (int(s[:len(s)//2]), int(s[len(s)//2 :]))
+    
+#     # If neither of above rules
+#     return stone * 2024
+    
+# @cache
+# def count(stone, stepsRem) :
+#     if stepsRem == 0 :              # No blinks remaining, just return the stone
+#         return 1
+    
+#     if stone == 0 :
+#         return count(anotherRule(stone), stepsRem - 1)
 
-# Your function call inside a lambda or function wrapper
-execution_time = timeit.timeit(lambda: sum(count(stone, 75) for stone in stones), number=1)
+#     # If stone is even-length string
+#     s = str(stone)
+#     if len(s) % 2 == 0 :
+#         l, r = anotherRule(stone)
+#         return count(l, stepsRem - 1) + count(r, stepsRem - 1)
+    
+#     # If neither of above rules
+#     return count(anotherRule(stone), stepsRem - 1)
 
-print(f"Execution time: {execution_time:.4f} seconds")
+# execution_time = timeit.timeit(lambda: sum(count(stone, 75) for stone in stones), number=1)
+
+# print(f"Execution time: {execution_time:.4f} seconds")
